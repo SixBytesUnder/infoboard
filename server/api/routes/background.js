ac9f509078825b54285453d1d8377f0a24905f3a
SixBytesUnder <SixBytesUnder@users.noreply.github.com>
2022-02-05 16:45:55 +0000

    Code cleanup

--- server/api/routes/background.js
+++ server/api/routes/background.js
@@ -75,33 +75,6 @@ router.get('/backgrounds/*', (req, res) => {
 })
 
 // path to get the file from outside of root project directory
-router.get('/asdbackground/:file(*)', (req, res) => {
-	// console.log(JSON.stringify(req.headers))
-	// console.log(req.headers['user-agent'].includes('Safari'))
-	// headers.add("Content-Length", contentLength);
-	// headers.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileSize);
-	// console.log(req.get('Range'))
-	// console.log(req.range())
-	const fullPath = path.join(mainDir, req.params.file)
-	const s = fs.createReadStream(fullPath)
-	s.on('open', function() {
-		if (req.params.file.split('.').pop().toLowerCase() === 'mp4') {
-			res.set('Content-Type', 'video/mp4')
-			// res.set('Content-Length', fs.statSync(fullPath).size)
-			// res.set('Content-Range', 'bytes 0-1024/' + fs.statSync(fullPath).size)
-			// res.set('Accept-Ranges', 'bytes')
-			// res.status(206)
-		} else {
-			res.set('Content-Type', 'image/jpeg')
-		}
-		s.pipe(res)
-	})
-	s.on('error', function() {
-		res.set('Content-Type', 'text/plain')
-		res.status(404).end(`File: ${fullPath} counld not be found`)
-	})
-})
-
 router.get('/background/:file(*)', (req, res) => {
 	const fullPath = path.join(mainDir, req.params.file)
 	const fileSize = fs.statSync(fullPath).size
